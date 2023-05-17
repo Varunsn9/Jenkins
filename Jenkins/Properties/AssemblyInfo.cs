824d60aed9df281a3ecf28af104a67dc16ae4997
Varunsn9 <104562883+Varunsn9@users.noreply.github.com>
2023-05-17 11:02:52 +0530

    Add project files.

---
+++ Jenkins/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Jenkins")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jenkins")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfcdc903-180c-4152-b187-a2dac09ce8bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Jenkins/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace Jenkins
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("TestJenkins")]
+
+        public void OrgCreation()
+        {
+            IWebDriver driver = new ChromeDriver();
+            string random = "" + RandomNum();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl("http://localhost:8888/");
+            driver.FindElement(By.Name("user_name")).SendKeys("admin");
+            driver.FindElement(By.Name("user_password")).SendKeys("admin");
+            driver.FindElement(By.Id("submitButton")).Click();
+            driver.FindElement(By.XPath("//a[.='Leads']/parent::td/following-sibling::td/a[.='Organizations']")).Click();
+            driver.FindElement(By.XPath("//img[@title='Create Organization...']")).Click();
+            driver.FindElement(By.Name("accountname")).SendKeys("NewOrg" + random);
+            driver.FindElement(By.XPath("//input[@title='Save [Alt+S]']")).Click();
+            Thread.Sleep(1000);
+
+        }
+        public int RandomNum()
+        {
+            Random ran=new Random();
+            int num=ran.Next(1, 100);
+            return num;
+        }
+    }
+}
